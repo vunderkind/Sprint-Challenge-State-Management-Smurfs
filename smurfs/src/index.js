ba65d6b78cc2666a96f4c681fecd7bf5db253de7
vunderkind <justin.irabor@gmail.com>
2019-10-11 09:31:28 +0100

    set up Redux on index.js and test reducer on src/reducers/reducer.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,21 @@
+//redux setup here
+import logger from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';	
+import {smurfReducer} from './reducers/reducer.js';
+
+//React-main stuff here
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//store + middleWare logic
+const store = createStore(
+    smurfReducer, 
+    applyMiddleware(thunk,logger)
+    );
+
+ReactDOM.render(
+<Provider store={store}><App />
+</Provider>, document.getElementById("root"));
---
+++ smurfs/src/reducers/reducer.js
@@ -0,0 +1,5 @@
+//basic Redux reducer wiring happening here
+
+export const smurfReducer = () => {
+    return 2*2;
+};
